2fc9e87eaa3449f092cf14be6c043623ee699677
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-06-27 14:11:15 +0200

    added test

---
+++ contracts/Test.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.5.6;
+
+import 'zos-lib/contracts/Initializable.sol';
+
+contract Test is Initializable {
+    function initialize()
+        external
+        initializer
+    {
+    }
+}
---
+++ test/test.js
@@ -0,0 +1,91 @@
+/* web3, artifacts */
+const {
+    deployContracts,
+    upgradeContracts,
+    zosCreate,
+    confirmUpgrade,
+} = require('../src/dori')
+
+function evaluateContracts({
+   contracts
+} = {}) {
+    return contracts;
+}
+
+async function initializeContracts(
+    contracts,
+    roles,
+    verbose = true
+) {
+    const addressBook = {}
+
+    // WARNING!
+    // use this only when deploying a selective portion of the contracts
+    // Only use this if you know what you do, otherwise it can break the contracts deployed
+    const proxies = {}
+
+    // returns either the address from the address book or the address of the manual set proxies
+    const getAddress = (contract) => {
+        return addressBook[contract] || proxies[contract]
+    }
+
+    if (contracts.indexOf('Test') > -1) {
+        addressBook['Test'] = zosCreate(
+            'Test',
+            null,
+            verbose
+        )
+    }
+
+    return addressBook
+}
+
+async function setupContracts() {
+
+}
+
+const verbose = true;
+
+module.exports = async (cb) => {
+    // deploy the contract
+    await deployContracts({
+        web3,
+        artifacts,
+        evaluateContracts,
+        initializeContracts,
+        setupContracts,
+        contracts: [
+            'Test'
+        ],
+        forceWalletCreation: true,
+        deeperClean: true,
+        testnet: false,
+        verbose
+    })
+        .catch(err => cb(err))
+
+    // upgrade the contract
+    const taskBook = await upgradeContracts({
+        web3,
+        artifacts,
+        evaluateContracts,
+        contracts: [
+            'Upgrade:Test'
+        ],
+        verbose
+    })
+        .catch(err => cb(err))
+
+    const accounts = await web3.eth.getAccounts()
+
+    // confirm the upgrade
+    await confirmUpgrade(
+        web3,
+        taskBook['Test'],
+        accounts[2],
+        verbose
+    )
+
+    cb()
+}
+
