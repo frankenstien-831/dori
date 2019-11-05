48cef20a31fa69a1e208842f7dd3736982e65fc1
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-11-05 09:59:27 +0100

    update documentation

--- test/test.js
+++ test/test.js
@@ -2,82 +2,14 @@
 const {
     deployContracts,
     upgradeContracts,
-    zosCreate,
     confirmUpgrade,
     audit,
     getAddresses
 } = require('../src/dori')
 
-function evaluateContracts({
-    contracts = []
-} = {}) {
-    return contracts && contracts.length > 0 ? contracts : [
-        'Test',
-        'EarlyOwner'
-    ]
-}
-
-async function initializeContracts({
-    contracts,
-    roles,
-    network,
-    verbose = true
-} = {}) {
-    const addressBook = {}
-
-    // WARNING!
-    // use this only when deploying a selective portion of the contracts
-    // Only use this if you know what you do, otherwise it can break the contracts deployed
-    const proxies = {}
-
-    // returns either the address from the address book or the address of the manual set proxies
-    /* eslint-disable-next-line no-unused-vars */
-    const getAddress = (contract) => {
-        return addressBook[contract] || proxies[contract]
-    }
-
-    if (contracts.indexOf('Test') > -1) {
-        addressBook.Test = zosCreate({
-            contract: 'Test',
-            network,
-            args: null,
-            verbose
-        })
-    }
-
-    if (contracts.indexOf('EarlyOwner') > -1) {
-        addressBook.EarlyOwner = zosCreate({
-            contract: 'EarlyOwner',
-            network,
-            args: [
-                roles.ownerWallet
-            ],
-            verbose
-        })
-    }
-
-    return addressBook
-}
-
-async function setupContracts({
-    addressBook,
-    artifacts,
-    roles,
-    verbose
-} = {}) {
-    if (verbose) {
-        /* eslint-disable-next-line no-console */
-        console.log('Setting up contracts')
-    }
-
-    const Test = await artifacts.require('Test')
-    const TestInstance = await Test.at(addressBook.Test)
-
-    await TestInstance.transferOwnership(
-        roles.ownerWallet,
-        { from: roles.deployer }
-    )
-}
+const evaluateContracts = require('./evaluateContracts')
+const initializeContracts = require('./initializeContracts')
+const setupContracts = require('./setupContracts')
 
 const verbose = true
 
